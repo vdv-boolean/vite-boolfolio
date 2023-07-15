a64dabbd77821f15751a9f37a464777490f101f4
vincenzodivita <divitavincenzo@icloud.com>
2023-07-15 16:26:56 +0200

    Import ProjectList component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script>
+import ProjectList from './components/ProjectList.vue';
 
+export default {
+    components: {
+        ProjectList,
+    }
+};
 
 </script>
 
 <template>
     <div>
         <h1>ciao</h1>
+        <ProjectList />
     </div>
 
 
 </template>
 
 <style lang=scss>
-@import 'bootstrap/scss/bootstrap'
+@import 'bootstrap/scss/bootstrap';
+
 </style>
