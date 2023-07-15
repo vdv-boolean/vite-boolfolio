7a0c77f570e229f00e7b6d56d904e68c30ec7679
vincenzodivita <divitavincenzo@icloud.com>
2023-07-15 17:03:50 +0200

    Add axios request and ad v-for for project titles

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,13 +1,27 @@
 <script>
+    import axios from 'axios';
     export default {
-
+        data() {
+            return {
+                arrProjects: [],
+            };
+        },
+        created() {
+            axios
+              .get('http://localhost:8000/api/projects')
+            .then(response => (this.arrProjects = response.data));
+        },
     }
 
 </script>
 
 <template>
 
-<h2>ciao</h2>
+<ul>
+    <li v-for="project in arrProjects" :key="project.id">
+        {{ project.title }}
+    </li>
+</ul>
 
 </template>
 
