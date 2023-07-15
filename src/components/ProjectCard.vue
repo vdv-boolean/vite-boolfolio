00635c02ed613da62226c04ed947071a6f66e75f
vincenzodivita <divitavincenzo@icloud.com>
2023-07-15 18:28:05 +0200

    Add component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,46 @@
+<script>
+    export default {
+        props: {
+            data: Object,
+        },
+    }
+
+</script>
+
+<template>
+
+<ul>
+    <li>
+        {{ data.title }}
+    </li>
+    <li>
+        {{ data.creation_date }}
+    </li>
+    <li>
+        {{ data.description }}
+    </li>
+    <li>
+        <a :href=data.link_github>
+            Github
+        </a>
+    </li>
+    <li>
+        Technologies:
+        <ul>
+            <li
+                v-for="tech in data.technologies" 
+                :key="tech.id"
+            >
+                {{ tech.name }}
+            </li>
+        </ul>
+    </li>
+
+</ul>
+
+</template>
+
+<style lang=scss scoped>
+
+
+</style>
